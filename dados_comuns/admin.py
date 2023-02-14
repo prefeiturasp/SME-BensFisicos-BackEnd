ccb0622df38f05dc324c7a435d4900bfe5756ca4
rayane maria dos santos <rayanemariadossantos@MacBook-Air-de-rayane.local>
2023-02-14 15:44:10 -0300

    Define modelo para unidade administrativa e configura admin

--- dados_comuns/admin.py
+++ dados_comuns/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from django import forms
+from dados_comuns.models import UnidadeAdministrativa
 
-# Register your models here.
+admin.site.register(UnidadeAdministrativa)
--- dados_comuns/models.py
+++ dados_comuns/models.py
@@ -1,3 +1,21 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+class UnidadeAdministrativa(models.Model):
+    codigo = models.CharField("Codigo", max_length=255, null=False, blank=False)
+    sigla = models.CharField("sigla", max_length=255, null=False, blank=False)
+    nome = models.CharField("nome", max_length=255, null=False, blank=False)
+    created_at = models.DateTimeField("Criado em", auto_now=True)
+    updated_at = models.DateTimeField("Atualizado em", auto_now=True, null=True, blank=True)
+
+    def __str__(self):
+        return '{} - {}'.format(self.codigo, self.sigla)
+
+    class Meta:
+        verbose_name = 'unidade administrativa'
+        verbose_name_plural = 'unidades administrativas'
+
+    def save(self, *args, **kwargs):
+        self.updated_at = datetime.now()
+        return super(UnidadeAdministrativa, self).save(*args, **kwargs)
