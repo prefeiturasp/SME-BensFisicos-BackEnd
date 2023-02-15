c806dce3bce3609c2312589aa393aa4b7b06a8a5
Luiz Henrique Priotto <luizh_priotto@hotmail.com>
2023-02-15 17:43:29 -0300

    Update base.py

--- config/settings/base.py
+++ config/settings/base.py
@@ -171,6 +171,7 @@ STATIC_URL = '/django_static/'
 # https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = [
     str(BASE_DIR.path('static')),
+    str(BASE_DIR.path('staticfiles')),
 ]
 
 # MEDIA
