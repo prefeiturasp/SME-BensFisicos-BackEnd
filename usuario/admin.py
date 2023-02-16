25acb24aa3aa699d628a92e5d35d1bf16654daf3
rayane maria dos santos <rayanemariadossantos@MacBook-Air-de-rayane.local>
2023-02-16 10:18:31 -0300

    fix: corrige criacao de usuario

--- usuario/admin.py
+++ usuario/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.auth.admin import UserAdmin
 from usuario.models import Usuario
-from django_admin_listfilter_dropdown.filters import DropdownFilter, RelatedDropdownFilter, ChoiceDropdownFilter
-from rangefilter.filters import DateRangeFilter, DateTimeRangeFilter, NumericRangeFilter
+from django_admin_listfilter_dropdown.filters import DropdownFilter
+from rangefilter.filters import DateRangeFilter
 
 
 class CustomUserModelAdmin(UserAdmin):
@@ -16,10 +16,11 @@ class CustomUserModelAdmin(UserAdmin):
         ('unidade_administrativa__sigla', DropdownFilter),
         ('date_joined', DateRangeFilter),
     )
+
     fieldsets = (('Acesso', {'fields': ('username', 'password')}), ('Informações pessoais', {'fields': ('nome', 'email', 'unidade_administrativa', )}),
+                 ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', )}), ('Datas importantes', {'fields': ('last_login', 'date_joined')}))
+    add_fieldsets = (('Acesso', {'fields': ('username', 'password1', 'password2')}), ('Informações pessoais', {'fields': ('nome', 'email', 'unidade_administrativa', )}),
                  ('Permissões', {'fields': ('is_active', 'is_staff', 'is_superuser', )}), ('Datas importantes', {'fields': ('last_login', 'date_joined')}))
-    add_fieldsets = fieldsets
 
 
-admin.site.unregister(Group)
 admin.site.register(Usuario, CustomUserModelAdmin)
