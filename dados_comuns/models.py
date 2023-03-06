94b8fc60803fd1cc54a2f280fb45fe716baee312
rayane maria dos santos <rayanemariadossantos@MacBook-Air-de-rayane.local>
2023-03-06 18:55:40 -0300

    Transfere logica de suporte atendimento para outro app

--- dados_comuns/models.py
+++ dados_comuns/models.py
@@ -3,9 +3,11 @@ from django.db import models
 
 
 class UnidadeAdministrativa(models.Model):
+    '''Classe que representa uma unidade administrativa'''
     codigo = models.CharField("Codigo", max_length=255, null=False, blank=False)
     sigla = models.CharField("sigla", max_length=255, null=False, blank=False)
     nome = models.CharField("nome", max_length=255, null=False, blank=False)
+    # controle
     created_at = models.DateTimeField("Criado em", auto_now=True)
     updated_at = models.DateTimeField("Atualizado em", auto_now=True, null=True, blank=True)
 
