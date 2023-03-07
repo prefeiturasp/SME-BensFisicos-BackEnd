9e81caf42d5d2e265c360aefc4bc37cd27bd3113
rayane maria dos santos <rayanemariadossantos@MacBook-Air-de-rayane.local>
2023-03-07 19:49:09 -0300

    Implementa ajustes para logica de input de horas dinamico

--- agendamento_suporte/admin.py
+++ agendamento_suporte/admin.py
@@ -1,6 +1,7 @@
+import datetime
+from datetime import timedelta
 from django import forms
 from django.contrib import admin
-from django.contrib.admin.widgets import AdminDateWidget
 
 from agendamento_suporte.models import AgendamentoSuporte, ConfigAgendaSuporte, DiaSemana, IntervaloHoras
 from agendamento_suporte.emails import envia_email_alerta_novo_agendamento
@@ -8,26 +9,47 @@ import nested_admin
 
 
 class AgendamentoSuporteForm(forms.ModelForm):
-    horarios = forms.Select()
+    data_agendada = forms.DateField(widget=forms.DateInput(
+        attrs={
+            'type': 'date',
+            'class': 'vDateField',
+            'id': 'id_data_agendada',
+            'onchange': 'onChangeDate();',
+            'min': datetime.datetime.today().strftime('%Y-%m-%d'),
+            'max': (datetime.datetime.today() + timedelta(days=6)).strftime('%Y-%m-%d')
+        }))
+
+    class Media:
+        js = ('js/agendamento_suporte/select_horas.js', )
+
+    def __init__(self, *args, **kwargs):
+        super(AgendamentoSuporteForm, self).__init__(*args, **kwargs)
+        if self.instance.pk:
+            self.initial['data_agendada'] = self.instance.data_agendada.strftime('%Y-%m-%d')
+            self.initial['hora_agendada'] = self.instance.hora_agendada.strftime('%H:%M')
 
 
 class AgendamentoSuporteAdmin(admin.ModelAdmin):
     model = AgendamentoSuporte
     list_display = ('id', 'agendado_por', 'data_agendada', 'hora_agendada', 'observacao', )
-    fields = ('data_agendada', 'horarios', 'observacao', )
+    fields = ('data_agendada', 'hora_agendada', 'observacao', )
     form = AgendamentoSuporteForm
-
-    def get_fieldsets(self, request, obj=None):
-        fieldsets = super(AgendamentoSuporteAdmin, self).get_fieldsets(request, obj)
-        fieldsets[0][1]['fields'] += ('horarios', )
-        return fieldsets
 
     def get_queryset(self, request):
         if request.user.is_operador_inventario:
             return AgendamentoSuporte.objects.filter(agendado_por=request.user)
         return AgendamentoSuporte.objects.all()
 
+    def get_value_of_custom_field(self, request):
+        time_str = request.POST.get('select_hora_agendada')
+        time_date = datetime.datetime.strptime(time_str, '%H:%M')
+        return time_date
+
     def save_model(self, request, obj, form, change):
+        # TODO trabalhar cenário em que usuário tenta agendar uma segunda reunião, sendo que ainda existe uma futura.
+        # possibilidades: Pedir permissão para substituir | Bloquear usuário de marcar
+        time_date = self.get_value_of_custom_field(request)
+        obj.hora_agendada = time_date
         if obj.id is None:
             obj.agendado_por = request.user
             envia_email_alerta_novo_agendamento()
