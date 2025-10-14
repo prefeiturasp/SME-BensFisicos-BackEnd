ca6f6e54c7d924e9665f6fed457f0a317049aec7
matheussacg <matheus.sacg@gmail.com>
2025-10-14 11:20:49 -0300

    feat: Adiciona suporte à exportação em PDF e ajusta queryset/formats de exportação

--- bem_patrimonial/admins/bem_patrimonial.py
+++ bem_patrimonial/admins/bem_patrimonial.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.db import models
 from django.db.models import Q, OuterRef, Subquery
-from bem_patrimonial.models import (BemPatrimonial, StatusBemPatrimonial, UnidadeAdministrativaBemPatrimonial)
-from bem_patrimonial.constants import APROVADO
+from bem_patrimonial.models import (
+    BemPatrimonial,
+    StatusBemPatrimonial,
+    UnidadeAdministrativaBemPatrimonial,
+)
+from bem_patrimonial.formats import PDFFormat
 from import_export.admin import ImportExportModelAdmin
 from import_export import resources, fields
 from rangefilter.filters import DateRangeFilter
+from import_export.formats.base_formats import CSV, XLS, XLSX, HTML
 
 
 class UnidadeAdministrativaBemPatrimonialInline(admin.TabularInline):
@@ -16,50 +21,84 @@ class UnidadeAdministrativaBemPatrimonialInline(admin.TabularInline):
 class StatusBemPatrimonialInline(admin.TabularInline):
     model = StatusBemPatrimonial
     extra = 0
-    readonly_fields = ('atualizado_por', 'atualizado_em', )
+    readonly_fields = (
+        "atualizado_por",
+        "atualizado_em",
+    )
 
 
 class BemPatrimonialResource(resources.ModelResource):
     quantidade_unidade = fields.Field(
-        column_name='quantidade',
-        attribute='quantidade_unidade',
+        column_name="quantidade",
+        attribute="quantidade_unidade",
     )
 
     class Meta:
         model = BemPatrimonial
-        fields = ('id', 'status', 'nome', 'data_compra_entrega', 'origem',
-                  'marca', 'modelo', 'descricao', 'quantidade_unidade', 'valor_unitario',
-                  'numero_processo', 'autorizacao_no_doc_em', 'numero_nibpm', 'numero_cimbpm',
-                  'numero_patrimonial', 'localizacao', 'numero_serie', 'criado_por__nome', 'criado_em', )
+        fields = (
+            "id",
+            "status",
+            "nome",
+            "data_compra_entrega",
+            "origem",
+            "marca",
+            "modelo",
+            "descricao",
+            "quantidade_unidade",
+            "valor_unitario",
+            "numero_processo",
+            "autorizacao_no_doc_em",
+            "numero_nibpm",
+            "numero_cimbpm",
+            "numero_patrimonial",
+            "localizacao",
+            "numero_serie",
+            "criado_por__nome",
+            "criado_em",
+        )
         export_order = fields
 
 
 class BemPatrimonialAdmin(ImportExportModelAdmin):
     model = BemPatrimonial
-    list_display = ('id', 'status', 'descricao', 'criado_por', 'criado_em', )
-    search_fields = ('nome', 'descricao', 'marca', 'modelo', 'localizacao', 'numero_processo', )
-    search_help_text = 'Pesquise por nome, descrição, marca, modelo, localização ou número de processo.'
+    list_display = (
+        "id",
+        "status",
+        "descricao",
+        "criado_por",
+        "criado_em",
+    )
+    search_fields = (
+        "nome",
+        "descricao",
+        "marca",
+        "modelo",
+        "localizacao",
+        "numero_processo",
+    )
+    search_help_text = "Pesquise por nome, descrição, marca, modelo, localização ou número de processo."
     resource_class = BemPatrimonialResource
 
-    list_filter = (
-        'status',
-        ('criado_em', DateRangeFilter)
+    list_filter = ("status", ("criado_em", DateRangeFilter))
+
+    readonly_fields = (
+        "status",
+        "criado_por",
+        "criado_em",
     )
 
-    readonly_fields = ('status', 'criado_por', 'criado_em',)
-
     fields = (
-        'status',
-        'nome',
-        'descricao',
-        ('quantidade', 'valor_unitario'),
-        ('marca', 'modelo'),
-        ('data_compra_entrega'),
-        ('origem', 'numero_processo'),
-        'autorizacao_no_doc_em',
-        ('numero_nibpm', 'numero_cimbpm', 'numero_patrimonial'),
-        'localizacao',
-        'numero_serie',
+        "status",
+        "nome",
+        "descricao",
+        ("quantidade", "valor_unitario"),
+        ("marca", "modelo"),
+        ("data_compra_entrega"),
+        ("origem", "numero_processo"),
+        "autorizacao_no_doc_em",
+        ("numero_nibpm", "numero_cimbpm", "numero_patrimonial"),
+        "localizacao",
+        "numero_serie",
     )
 
     inlines = [StatusBemPatrimonialInline, UnidadeAdministrativaBemPatrimonialInline]
@@ -76,26 +115,53 @@ class BemPatrimonialAdmin(ImportExportModelAdmin):
         queryset = BemPatrimonial.objects.all()
         if request.user.is_operador_inventario:
             return queryset.filter(
-                Q(unidadeadministrativabempatrimonial__quantidade__gt=0) &
-                Q(unidadeadministrativabempatrimonial__unidade_administrativa=request.user.unidade_administrativa)
-            ).distinct('id')
+                Q(unidadeadministrativabempatrimonial__quantidade__gt=0)
+                & Q(
+                    unidadeadministrativabempatrimonial__unidade_administrativa=request.user.unidade_administrativa
+                )
+            ).distinct("id")
         return queryset
 
     def get_export_queryset(self, request):
-        queryset = BemPatrimonial.objects.filter(status=APROVADO)
+        queryset = super().get_export_queryset(request)
+
         if request.user.is_operador_inventario:
-            queryset = queryset.filter(
-                Q(unidadeadministrativabempatrimonial__quantidade__gt=0) &
-                Q(unidadeadministrativabempatrimonial__unidade_administrativa=request.user.unidade_administrativa)
-            ).distinct('id').annotate(
-                quantidade_unidade=Subquery(
-                    UnidadeAdministrativaBemPatrimonial.objects.filter(
-                        bem_patrimonial=OuterRef('id'),
-                        unidade_administrativa=request.user.unidade_administrativa,
-                    ).values('quantidade')[:1],
-                    output_field=models.IntegerField()
-                ))
+            queryset = (
+                queryset.filter(
+                    Q(unidadeadministrativabempatrimonial__quantidade__gt=0)
+                    & Q(
+                        unidadeadministrativabempatrimonial__unidade_administrativa=request.user.unidade_administrativa
+                    )
+                )
+                .distinct("id")
+                .annotate(
+                    quantidade_unidade=Subquery(
+                        UnidadeAdministrativaBemPatrimonial.objects.filter(
+                            bem_patrimonial=OuterRef("id"),
+                            unidade_administrativa=request.user.unidade_administrativa,
+                        ).values("quantidade")[:1],
+                        output_field=models.IntegerField(),
+                    )
+                )
+            )
+
         return queryset
+
+    def get_export_formats(self):
+        return [CSV, XLSX, XLS, HTML, PDFFormat]
+
+    def get_resource_kwargs(self, request, **kwargs):
+        rk = super().get_resource_kwargs(request, **kwargs)
+        rk["request"] = request
+        self._current_export_request = request
+        return rk
+
+    def get_export_data(self, file_format, queryset, *args, **kwargs):
+        if isinstance(file_format, PDFFormat):
+            file_format._export_request = getattr(self, "_current_export_request", None)
+            file_format._export_queryset = queryset
+
+        return super().get_export_data(file_format, queryset, *args, **kwargs)
 
     def save_formset(self, request, form, formset, change):
         if formset.model is StatusBemPatrimonial:
